da4f6577543790bb36f8343ff11c9c52bf9af8f1
OsvaldoRicardo <osvaldo.machava95@gmail.com>
2017-11-11 17:08:06 +0200

    Modelo e migrate Valor da multa

---
+++ database/migrations/2017_11_11_145707_create_valor_multas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateValorMultasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('valor_multas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('valor');
+            $table->string('descricao',100)->default('Null');
+            $table->boolean('status')->default(1);
+            $table->integer('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('valor_multas');
+    }
+}
